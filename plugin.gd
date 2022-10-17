4308b84bb573a6719caf7ca16207e2b29b7f3b08
samuelbigos <bigos.sam+github@googlemail.com>
2022-10-17 17:53:46 +0100

    Use is_instance_valid instead of a basic null check to fix errors when re-creating NavigationPolygon.

--- plugin.gd
+++ plugin.gd
@@ -17,11 +17,11 @@ func edit(object):
 	nav_poly_to_edit = object
 
 func make_visible(visible):
-	if visible and panel == null:
+	if visible and not is_instance_valid(panel):
 		panel = EditPanel.instance()
 		add_control_to_bottom_panel(panel, "NavmeshGen")
 		panel.setup(nav_poly_to_edit)
-	elif panel != null:
+	elif is_instance_valid(panel):
 		remove_control_from_bottom_panel(panel)
 		panel.shutdown()
 		panel.queue_free()
